e5b844ed7899696161de49e58989ed9213dffc9d
Vanessa Viglucci <vanessaviglucci@Vanessas-MBP.lan>
2024-04-12 22:39:19 -0400

    Temp code

--- web_app/app.py
+++ web_app/app.py
@@ -1,8 +1,9 @@
 import os
 import datetime
+import tempfile
 from machine_learning_client import speech_recog
+import speech_recognition as sr
 from flask import Flask, render_template, request, redirect, url_for, make_response, jsonify
-
 import pymongo
 from dotenv import load_dotenv
 
@@ -47,12 +48,40 @@ def create():
     else:
         return render_template('create.html')
 
-
-
 #audio handler - to be implemented
 @app.route("/audio", methods=['GET', 'POST'])
 def audio():
-    pass
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"status": "error", "message": "No file part"})
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"status": "error", "message": "No selected file"})
+
+        if file and allowed_file(file.filename):
+            # Save the uploaded file to a temporary location
+            temp_audio = tempfile.NamedTemporaryFile(delete=False)
+            file.save(temp_audio.name)
+
+            recognizer = sr.Recognizer()
+            with sr.AudioFile(temp_audio.name) as source:
+                audio_data = recognizer.record(source)  # Record the entire audio file
+
+            try:
+                recognized_text = recognizer.recognize_google(audio_data)
+                return render_template("translate_audio.html", recognized_text=recognized_text)
+                #return jsonify({"status": "success", "text": recognized_text})
+            except sr.UnknownValueError:
+                return jsonify({"status": "error", "message": "Unable to recognize the audio"})
+            except sr.RequestError as e:
+                return jsonify({"status": "error", "message": "Google Speech Recognition service error"})
+
+        else:
+            return jsonify({"status": "error", "message": "Invalid file type"})
+    else: 
+        return render_template('translate_audio.html')
 
 #show past data
 @app.route("/show", methods=['GET', 'POST'])
