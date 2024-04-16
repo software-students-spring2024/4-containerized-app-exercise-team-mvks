90893231ec7e8d6f1652d2e74ba91561b163f9ce
Linkun Wan <lw2861@nyu.edu>
2024-04-15 22:08:57 -0400

    Combined All changes

--- aa.py
+++
@@ -1,6 +0,0 @@
-import speech_recognition as sr
-recognizer = sr.Recognizer()
-with sr.Microphone() as source:
-    audio = recognizer.listen(source)
-    text = recognizer.recognize_google(audio)
-    print(text)
